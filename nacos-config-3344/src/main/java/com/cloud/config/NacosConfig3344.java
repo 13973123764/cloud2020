24f7b2b41cadd329f7f22190c72ed167bcbe3443
13973123764@163.com <13973123764@163.com>
2020-07-15 15:15:50 +0800

    first commit

---
+++ nacos-config-3344/src/main/java/com/cloud/config/NacosConfig3344.java
@@ -0,0 +1,24 @@
+package com.cloud.config;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.concurrent.TimeUnit;
+
+@SpringBootApplication
+public class NacosConfig3344 {
+
+    public static void main(String[] args) throws InterruptedException {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(NacosConfig3344.class, args);
+        while(true) {
+            String userName = applicationContext.getEnvironment().getProperty("user.name");
+            String userAge = applicationContext.getEnvironment().getProperty("user.age");
+            //Get the current deployment environment
+            String currentEnv = applicationContext.getEnvironment().getProperty("current.env");
+            System.err.println("in "+currentEnv+" enviroment; "+"user name :" + userName + "; age: " + userAge);
+            TimeUnit.SECONDS.sleep(1);
+        }
+    }
+}
